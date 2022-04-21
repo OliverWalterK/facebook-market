6a032f68e41945968d6f1971490db7b65b342050
Oliver <kollo.oliver@gmail.com>
2022-04-21 14:29:59 +0100

    updated the clean_images.py but still missing the documentation

--- clean_images.py
+++ clean_images.py
@@ -1,45 +1,50 @@
+from sqlalchemy import create_engine
 from PIL import Image
-import glob
+import os
 
-list_img = glob.glob('images/*.jpg')
+path = "images/"
+dirs = os.listdir(path)
+
+def connect_to_db():
+    host = 'products.c8k7he1p0ynz.us-east-1.rds.amazonaws.com'
+    password = 'aicore2022!'
+    port = 5432
+    user = 'postgres'
+    database = 'postgres'
+    api_type = 'psycopg2'
+    engine = create_engine(f'postgresql+{api_type}://{user}:{password}@{host}:{port}/{database}')
+    engine.connect()
+    return engine
 
 ## This code will find out what the smallest resolution is from all the images.
-# def minimum_dimensions():
-#     min_width = 20000
-#     min_height = 20000
-#     for img in list_img:
-#         size = Image.open(img).size
-#         width = size[0]
-#         height = size[1]
-#         if width < min_width:
-#             min_width = width
-#         if height < min_height:
-#             min_height = height
-#     print(min_width)
-#     print(min_height)
+def minimum_dimensions():
+    min_width = 20000
+    min_height = 20000
+    for img in dirs:
+        size = Image.open(img).size
+        width = size[0]
+        height = size[1]
+        if width < min_width:
+            min_width = width
+        if height < min_height:
+            min_height = height
+    print(min_width)
+    print(min_height)
 
-# final_size = 512
-# img = Image.open(list_img[0])
-# black_img = Image.new(mode='RGB', size=(final_size, final_size))
-# prev_size = img.size
-# max_dim = max(img.size)
-# ratio = final_size / max_dim
-# new_img_size = (int(prev_size[0] * ratio), int(prev_size[1] * ratio))
-# new_img = img.resize(new_img_size)
-# black_img.paste(new_img, ((final_size - new_img_size[0]) // 2, (final_size - new_img_size[1]) // 2))
-# black_img.show
+def resize_image(final_size, im):
+    size = im.size
+    ratio = float(final_size) / max(size)
+    new_image_size = tuple([int(x*ratio) for x in size])
+    im = im.resize(new_image_size, Image.ANTIALIAS)
+    new_im = Image.new("RGB", (final_size, final_size))
+    new_im.paste(im, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
+    return new_im
 
-min_width = 512
-final_size = 512
-for n, img in enumerate(list_img[:50], 1):
-    next_img = Image.open(img)
-    if next_img.mode != 'RGB':
-        next_img.mode = 'RGB'
-    black_img = Image.new(mode='RGB', size=(final_size, final_size))
-    # prev_size = img.size
-    # max_dim = max(img.size)
-    # ratio = final_size / max_dim
-    # new_img_size = (int(prev_size[0] * ratio), int(prev_size[1] * ratio))
-    # new_img = img.resize(new_img_size)
-    # black_img.paste(new_img, ((final_size - new_img_size[0]) // 2, (final_size - new_img_size[1]) // 2))
-    # black_img.save(f'image_{n}.jpg')
+if __name__ == '__main__':
+    final_size = 512
+    for n, item in enumerate(dirs[:5], 1):
+        im = Image.open('images/' + item)
+        if im.mode != 'RGB':
+            im.mode = 'RGB'
+        new_im = resize_image(final_size, im)
+        new_im.save(f'{n}_resized.jpg')
